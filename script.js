671be589258f8d36a9f0baa18f42f0b9c41a8dda
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-03-27 11:04:56 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    console.log("Web is ready");
+});
+ 
+
+/////////////////////////////////////////
+//            INDEX.html
+/////////////////////////////////////////
+function showCategory(){
+    $(".category-list").toggle();
+}
+function showtag(){
+    $(".tag-list").toggle();
+}
+function showPrice(){
+    $(".price-list").toggle();
+}
+function showOnsale(){
+    $(".onsale-list").toggle();
+}
+function showRating(){
+    $(".rating-list").toggle();
+}
+function showDate(){
+    $(".date-list").toggle();
+}
+
+
+/////////////////////////////////////////
+//            DASHBOARD.html
+/////////////////////////////////////////
